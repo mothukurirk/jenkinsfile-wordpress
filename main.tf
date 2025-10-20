29244e94e86efc973d8ba45cfd297284904d5300
Radha <68968301+mothukurirk@users.noreply.github.com>
2025-10-20 15:06:43 -0400

    Update S3 bucket resource naming convention

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "jenkins-terraform-demo-bucket-${random_id.rand.hex}"
-}
-
 resource "random_id" "rand" {
   byte_length = 4
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "jenkins-terraform-demo-${random_id.rand.hex}"
 }
